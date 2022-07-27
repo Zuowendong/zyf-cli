3e6b4309d6f6cdf677b9e1c27305d155a08d7c7e
zuowendong <17326195691@163.com>
2022-07-27 14:37:14 +0800

    feat: 通过inquirer交互式生成inputConfig

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 // fs
 import fs from "fs";
+import inquirer from "inquirer";
 import createIndexTemplate from "./createIndexTemplate.js";
 import createPackageTemplate from "./createPackageTemplate.js";
 
 // input
 // process
 // output
+
+const r = await inquirer.prompt([
+    /* Pass your questions in here */
+    {
+        type: "input",
+        name: "packageName",
+        message: "set package name",
+        validate(val) {
+            if (val) return true;
+            return "please enter package name";
+        },
+    },
+    {
+        type: "input",
+        name: "port",
+        default() {
+            return 8080;
+        },
+        message: "set port number",
+    },
+    {
+        type: "checkbox",
+        name: "middleware",
+        choices: [{ name: "koaStatic" }, { name: "koaRouter" }],
+    },
+]);
+
+console.log(r);
 
 const inputConfig = {
     packageName: "hei",
@@ -17,14 +46,17 @@ const inputConfig = {
 };
 
 // 1. 创建文件夹 -> hei
-fs.mkdirSync(getRootPath());
-// 2. 创建入口文件 -> index.js
-fs.writeFileSync(`${getRootPath()}/index.js`, createIndexTemplate(inputConfig));
-// 3. 创建package.json
-fs.writeFileSync(`${getRootPath()}/package.json`, createPackageTemplate(inputConfig));
-// 4. 安装依赖
-// Todo
+// fs.mkdirSync(getRootPath());
+// // 2. 创建入口文件 -> index.js
+// fs.writeFileSync(`${getRootPath()}/index.js`, createIndexTemplate(inputConfig));
+// // 3. 创建package.json
+// fs.writeFileSync(
+//     `${getRootPath()}/package.json`,
+//     createPackageTemplate(inputConfig)
+// );
+// // 4. 安装依赖
+// // Todo
 
-function getRootPath() {
-    return "./hei";
-}
+// function getRootPath() {
+//     return "./hei";
+// }
