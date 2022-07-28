0b8c56065384521ac9602d6da86884794092b924
zuowendong <17326195691@163.com>
2022-07-28 14:26:22 +0800

    feat: 使用chalk美化终端输出

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,7 @@
 // fs
 import fs from "fs";
 import { execa } from "execa";
-
+import chalk from 'chalk'
 import createIndexTemplate from "./createIndexTemplate.js";
 import createPackageTemplate from "./createPackageTemplate.js";
 import question from "./question/index.js";
@@ -16,16 +16,21 @@ const answer = await question();
 const config = createConfig(answer);
 
 // 1. 创建文件夹 -> hei
+console.log(chalk.blue(`创建文件夹 -> ${config.packageName}`));
 fs.mkdirSync(getRootPath());
+
 // 2. 创建入口文件 -> index.js
+console.log(chalk.blue(`创建入口文件 -> index.js`));
 fs.writeFileSync(`${getRootPath()}/index.js`, createIndexTemplate(config));
+
 // 3. 创建package.json
+console.log(chalk.blue(`创建package.json`));
 fs.writeFileSync(
     `${getRootPath()}/package.json`,
     createPackageTemplate(config)
 );
 // 4. 安装依赖 ： 官方库：child-process子进程    第三方库： execa (child-process子进程 库的封装)
-// Todo
+console.log(chalk.blue(`安装依赖`));
 execa("npm install", {
     cwd: getRootPath(),
     stdio: [2, 2, 2],
